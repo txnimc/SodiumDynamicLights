0a7c352cd499b79b372dd4d7d434fbf1a1029c86
Toni <vicerust@gmail.com>
2024-09-24 11:07:58 -0400

    revert SC

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version "0.5-alpha.8"
+	id("dev.kikugie.stonecutter") version "0.5-alpha.4"
 }
 
 extensions.configure<StonecutterSettings> {
